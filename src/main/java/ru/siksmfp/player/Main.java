b941b69b05247e3ce0303dfff592335a22e202cb
parkito <artyom-karnov@yandex.ru>
2019-05-17 21:41:59 +0300

    Initial commit

---
+++ src/main/java/ru/siksmfp/player/Main.java
@@ -0,0 +1,10 @@
+package ru.siksmfp.player;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+//    export JAVA_HOME = '/Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home'-- >
+//    export JAVA_HOME='/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home'
